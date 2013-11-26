2d4c73172bde175dc2f177714207f8b489b4e37d
antonioasaro <antonioasaro@users.noreply.github.com>
2013-11-26 12:39:12 -0800

    Better spacing and fonts.

--- src/main.c
+++ src/main.c
@@ -132,19 +132,19 @@ void handle_init(void) {
   	frm_layer = text_layer_create(GRect(5, 10,  135, 30));
   	text_layer_set_text_color(frm_layer, GColorBlack);
   	text_layer_set_background_color(frm_layer, GColorClear);
-  	text_layer_set_font(frm_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD));
+  	text_layer_set_font(frm_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
   	layer_add_child(root_layer, text_layer_get_layer(frm_layer));
 
 	who_layer = text_layer_create(GRect(5, 40,  135, 30));
   	text_layer_set_text_color(who_layer, GColorBlack);
   	text_layer_set_background_color(who_layer, GColorClear);
-  	text_layer_set_font(who_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD));
+  	text_layer_set_font(who_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
   	layer_add_child(root_layer, text_layer_get_layer(who_layer));
 
 	msg_layer = text_layer_create(GRect(5, 60,  135, 60));
   	text_layer_set_text_color(msg_layer, GColorBlack);
   	text_layer_set_background_color(msg_layer, GColorClear);
-  	text_layer_set_font(msg_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD));
+  	text_layer_set_font(msg_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
   	layer_add_child(root_layer, text_layer_get_layer(msg_layer));
 
 	cmd_layer = text_layer_create(GRect(5, 110, 135, 40));
